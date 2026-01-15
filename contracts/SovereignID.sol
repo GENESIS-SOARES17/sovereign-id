af0de38619b3a712e990bd62f67bab16bd5e153b
EDGAR SOARES DA SILVA <132958606+GENESIS-SOARES17@users.noreply.github.com>
2026-01-14 21:25:13 -0300

    Create SovereignID.sol

---
+++ contracts/SovereignID.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SovereignID is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
+
+    mapping(address => bool) public hasId;
+    mapping(uint256 => bytes32) public identityHash;
+    mapping(address => uint256) public reputation;
+
+    event IdentityMinted(address indexed user, uint256 tokenId);
+    event ReputationUpdated(address indexed user, uint256 newScore);
+
+    constructor() ERC721("SovereignID", "SID") {}
+
+    function mint(bytes32 _identityHash) external {
+        require(!hasId[msg.sender], "Identity already exists");
+
+        _tokenIdCounter++;
+        uint256 tokenId = _tokenIdCounter;
+
+        _safeMint(msg.sender, tokenId);
+
+        hasId[msg.sender] = true;
+        identityHash[tokenId] = _identityHash;
+        reputation[msg.sender] = 1;
+
+        emit IdentityMinted(msg.sender, tokenId);
+    }
+
+    // Soulbound: block transfers
+    function _transfer(
+        address,
+        address,
+        uint256
+    ) internal pure override {
+        revert("SovereignID is soulbound");
+    }
+
+    function hasSovereignId(address user) external view returns (bool) {
+        return hasId[user];
+    }
+
+    function updateReputation(address user, uint256 newScore) external onlyOwner {
+        require(hasId[user], "No Sovereign ID");
+        reputation[user] = newScore;
+        emit ReputationUpdated(user, newScore);
+    }
+}
+Add SovereignID soulbound contract
